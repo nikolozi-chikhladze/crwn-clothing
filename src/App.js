2d47896e3e027473cfe05e81cd57f9818f272e72
nikolozi-chikhladze <49709585+nikolozi-chikhladze@users.noreply.github.com>
2021-08-19 13:20:57 +0400

    Implement firebase utils, included ability to store authenticated users into the firestore database, and sign up functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
 
@@ -20,9 +20,24 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log('user: ', user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => console.log(this.state))
+        });
+      } else {
+        this.setState({
+          currentUser: userAuth
+        })
+      }
+      // this.setState({ currentUser: user });
+      // console.log('user: ', user);
     });
   }
 
